9327723d08f8a55ff78a5ca487491991ec295f19
Ryan Scofield <ryanscofield68@gmail.com>
2025-01-15 02:29:28 +1300

    FEAT: Updated TODOs

--- app/src/main/java/nz/ac/canterbury/seng303/healthtracking/MainApplication.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/healthtracking/MainApplication.kt
@@ -17,9 +17,10 @@ class MainApplication: Application() {
 
 /**
  * TODO:
- * - Sleep, Eat, Stats, Settings
  *
  * - Settings (1) : Easiest and has a reasonably big impact (>20 hrs)
  * - Sleep (2) : 2nd Easiest and is important (>20-30 hrs)
  * - Eat (3) : May be a lot of work, big impact too but depends on how extensive my implementation is (30-40 hrs)
+ * - Stats (4) : Important but has to wait on other features first, should be quite quick to implement (~20 hours)
+ * - Workout (5) : Fix small issues, allow for more customisation (measurement types, kg vs. lb, timer, etc)
  */
