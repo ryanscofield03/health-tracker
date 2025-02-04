3709eebf777efb0ca5c49624c79620a35d2a3ac8
Ryan Scofield <rsc104@uclive.ac.nz>
2025-02-04 20:29:20 +1300

    FEAT: Set up foundation of nutritional API data fetching - this currently is all mocked and will be for a while - also needs user to have api key in their system variables

--- app/src/main/java/com/healthtracking/app/services/NutritionalDataAPI.kt
+++ app/src/main/java/com/healthtracking/app/services/NutritionalDataAPI.kt
@@ -3,26 +3,36 @@ package com.healthtracking.app.services
 import com.healthtracking.app.BuildConfig
 import com.healthtracking.app.entities.Food
 
+/**
+ * Class for handling the nutritional data API
+ * e.g. fetching nutritional data for a given food item in a range of measurements
+ */
 class NutritionalDataAPI {
     val nutritionalApiKey = BuildConfig.NUTRITIONAL_API_KEY
 
+    /**
+     * Retrieve a list of nutritional data for a given food item in different measurements
+     * that make sense for the food item (e.g. egg should have 1 tbsp as an option)
+     */
     private fun getNutritionalDataList(searchQuery: String): List<Food> {
         // actual implementation should try and match searchQuery to an actual item (e.g. brea -> bread)
 
         // actual implementation should have some debouncing to prevent too many requests
 
         return listOf(
-            getNutritionalData("$searchQuery (100g)"),
-            getNutritionalData("$searchQuery (1 cup)"),
-            getNutritionalData("$searchQuery (1 tbsp)"),
-            getNutritionalData("$searchQuery (1 tsp)")
-            // extend to allow for something like 1 egg or 1 chicken breast (1 unit)
+            getNutritionalData(foodItem = searchQuery, measurement = "100g"),
+            getNutritionalData(foodItem = searchQuery, measurement = "1 Unit"), // something like 1 egg or 1 chicken breast
+            getNutritionalData(foodItem = searchQuery, measurement = "1 Cup"),
+            getNutritionalData(foodItem = searchQuery, measurement = "1 Tbsp"),
         )
     }
 
-    private fun getNutritionalData(foodItem: String): Food {
+    /**
+     * Get the nutritional data for a given food item in its measurement
+     */
+    private fun getNutritionalData(foodItem: String, measurement: String): Food {
         return Food(
-            name = foodItem,
+            name = "$foodItem + ($measurement)",
             calories = 100f,
             protein = 10f,
             carbohydrates = 10f,
