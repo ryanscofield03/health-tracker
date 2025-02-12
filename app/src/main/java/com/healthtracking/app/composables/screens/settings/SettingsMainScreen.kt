4a260c0ee90796a1dfcc9e89fccecee1b16a2780
Ryan Scofield <rsc104@uclive.ac.nz>
2025-02-12 16:55:15 +1300

    FEAT: Cleaned up settings imports

--- app/src/main/java/com/healthtracking/app/composables/screens/settings/SettingsMainScreen.kt
+++ app/src/main/java/com/healthtracking/app/composables/screens/settings/SettingsMainScreen.kt
@@ -11,14 +11,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
